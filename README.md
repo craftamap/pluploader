1ac6b9118c69fafb6ea00346604f43c0bef11af7
Siegel, Fabian <fabian.siegel@scandio.de>
2020-02-20 13:02:18 +0100

    Made script installable

---
+++ README.md
@@ -0,0 +1,5 @@
+## Scandio Pluploader
+
+Your convenient Confluence/Jira Server Plugin Uploader for the command line!
+
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sc-pluploader-fsiegel",
+    version="0.0.1",
+    author="Fabian Siegel, Scandio GmbH",
+    author_email="fabian.siegel@scandio.de",
+    description="CLI Confluence/Jira Plugin uploader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.scandio.de/users/fsiegel/repos/sc-pluploader/browse",
+    packages=["scripts"],
+    entry_points={
+        "console_scripts": ["pluploader=scripts.pluploader:main"],
+    }
+)
