5f8dccf0133a3739c053baa4f183292cd48b9fab
Fabian Siegel <fabian@livelyapps.com>
2020-04-01 18:24:41 +0200

    Add interactive mode

--- scripts/pluploader.py
+++ scripts/pluploader.py
@@ -1,6 +1,3 @@
-import requests
-from requests.auth import HTTPBasicAuth
-import json
 import time
 from tqdm import tqdm
 from colorama import Fore
@@ -31,48 +28,6 @@ SSS.    {Fore.WHITE}°°{Fore.YELLOW}    .SSS
 {Fore.LIGHTGREEN_EX}  ssss{Fore.YELLOW}sssss{Fore.RESET}
 """
 
-def main():
-    print(LOGO)
-    project_root = pathutil.find_maven_project_root(".")
-    config_locations = ["~/.pluprc"]
-
-    if project_root is not False:
-        config_locations.append(os.path.join(project_root, ".pluprc"))
-    p = configargparse.ArgParser(default_config_files=config_locations,
-                                 config_file_parser_class=configargparse.YAMLConfigFileParser)
-    p.add("--host", default="localhost")
-    p.add("--scheme", default="http")
-    p.add("--user", required=True)
-    p.add("--password", required=True)
-    p.add("--port", default="8090")
-    p.add("-f", "--file", type=configargparse.FileType("rb"))
-    args = p.parse_args()
-
-    request_base = upm.RequestBase(scheme=args.scheme, host=args.host, port=args.port,
-                                   user=args.user, password=args.password)
-    try:
-        token = upm.get_token(request_base)
-        files = {}
-        if args.file is None:
-            plugin_name = pathutil.get_jar_from_pom()
-            files.update({'plugin': open(f"target/{plugin_name}", 'rb')})
-        else:
-            files.update({'plugin': args.file})
-        with TqdmUpTo(total=100) as pbar:
-            pbar.update_to(0)
-            progress, previous_request = upm.upload_plugin(request_base, files, token)
-            while progress != 100:
-                progress, previous_request = upm.get_current_progress(request_base,
-                                                                      previous_request)
-                pbar.update_to(progress)
-        print("Plugin hochgeladen und "+("enabled" if previous_request["enabled"] else "disabled")+"!")
-    except Exception as e:
-        raise e
-
-
-
-
-
 class TqdmUpTo(tqdm):
     """Provides `update_to(n)` which uses `tqdm.update(delta_n)`."""
     def update_to(self, b=1, bsize=1, tsize=None):
@@ -87,6 +42,62 @@ class TqdmUpTo(tqdm):
         if tsize is not None:
             self.total = tsize
         self.update(b * bsize - self.n)  # will also set self.n = b * bsize
+
+
+def main():
+    print(LOGO)
+    project_root = pathutil.find_maven_project_root(".")
+    config_locations = ["~/.pluprc"]
+
+    if project_root is not False:
+        config_locations.append(os.path.join(project_root, ".pluprc"))
+    p = configargparse.ArgParser(default_config_files=config_locations,
+                                 config_file_parser_class=configargparse.YAMLConfigFileParser)
+    p.add("--host", default="localhost")
+    p.add("--scheme", default="http")
+    p.add("--user", required=True)
+    p.add("--password", required=True)
+    p.add("--port", default="8090")
+    p.add("-i", "--interactive", default=False, action='store_true')
+    p.add("-f", "--file", type=configargparse.FileType("rb"))
+    args = p.parse_args()
+    run(args)
+
+
+def run(args):
+    """ Actual code of the pluploader
+    """
+    request_base = upm.RequestBase(scheme=args.scheme, host=args.host, port=args.port,
+                                   user=args.user, password=args.password)
+    try:
+        files = {}
+        if args.file is None:
+            plugin = pathutil.get_jar_from_pom()
+            files.update({'plugin': plugin})
+        else:
+            files.update({'plugin': args.file})
+
+        print(f"{os.path.basename(files.get('plugin').name)} will be uploaded to {request_base.host}:{request_base.port}")
+        if(args.interactive):
+            confirm = input("Do you really want to upload and install the plugin? (y/N) ")
+            if confirm.lower() != "y":
+                exit(0)
+
+        token = upm.get_token(request_base)
+        with TqdmUpTo(total=100) as pbar:
+            pbar.update_to(0)
+            progress, previous_request = upm.upload_plugin(request_base, files, token)
+            while progress != 100:
+                progress, previous_request = upm.get_current_progress(request_base,
+                                                                      previous_request)
+                pbar.update_to(progress)
+                time.sleep(0.1)
+        print("Plugin hochgeladen und "+
+              ("enabled" if previous_request["enabled"] else "disabled")
+              +"!")
+    except Exception as e:
+        raise e
+
 
 
 
