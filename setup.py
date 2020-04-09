cff19eb4bc2d6ec73f4486b5cf656ecd335ee414
Fabian Siegel <fabian@livelyapps.com>
2020-04-09 14:19:21 +0200

    Updated information in setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pluploader",
-    version="0.0.1",
-    author="Fabian Siegel, Scandio GmbH",
-    author_email="fabian.siegel@scandio.de",
+    version="0.1.0",
+    author="Fabian Siegel, Lively Apps GmbH",
+    author_email="fabian@livelyapps.com",
     description="CLI Confluence/Jira Plugin uploader",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://git.scandio.de/users/fsiegel/repos/sc-pluploader/browse",
+    url="https://github.com/livelyapps/pluploader",
     packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": ["pluploader=scripts.pluploader:main"],
