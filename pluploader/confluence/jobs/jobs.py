f6f6f71d6b3758526d1aad25b30b02fbe4d115ec
Fabian Siegel <fabian@livelyapps.com>
2020-11-10 14:54:38 +0100

    superlinter: python

--- pluploader/confluence/jobs/jobs.py
+++ pluploader/confluence/jobs/jobs.py
@@ -125,11 +125,7 @@ class JobsScraper:
         response = self.session.get(request_url)
         return response.status_code == 200
 
-    def enable_job(
-        self,
-        job: Job,
-        token: typing.Optional[str] = None,
-    ) -> bool:
+    def enable_job(self, job: Job, token: typing.Optional[str] = None,) -> bool:
         if token is None:
             token = self.get_token()
         request_url = self.base_url.copy()
